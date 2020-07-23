484d58694a3849d838ed1802cc85d52870a8db7e
mariobalrod <marioballestero.rdg@gmail.com>
2020-07-23 14:51:40 +0200

    feat : firebase database configured

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import * as firebase from 'firebase';
+
+// Firebase Config
+import { firebaseConfig } from '../firebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 
 export default function App () {
+
+  const [ prueba, setPrueba ] = React.useState('');
+
+  React.useEffect(() => {
+    const nameRef = firebase.database().ref().child("name");
+    nameRef.on("value", (snapshot) => {
+      setData(snapshot.val());
+    });
+  }, []);
+
+  const setData = (data) => {
+    setPrueba(data);
+  }
+
   return (
     <div className="App">
+      <h1>{prueba}</h1>
     </div>
   );
 }
